c2a1e2f5bc67cdc26136107751e57b58102c5a60
Lucas <Lucadbalaca2002@gmail.com>
2025-06-25 01:22:05 -0300

    criação do siatema

---
+++ src/Routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login/>}></Route>
+                <Route path="/Home" element={<Home/>}></Route>
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './Login.css'
+
+function Login () {
+  const [Email, setEmail] = useState("");
+  const [Senha, setSenha] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Dados enviados:", Email,Senha);
+    //alert("Formulário enviado com sucesso!");
+    // Aqui você pode enviar os dados para um servidor, por exemplo.
+    navigate("/Home")
+  };
+
+  return (
+    <div className="Login">
+    <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <div className="input-field">
+        <input
+          type="email"
+          id="email"
+          placeholder="E-mail"
+          onChange={(e)=> setEmail(e.target.value)}
+        />
+      </div>     
+      <div className="input-field">
+        <input
+          type="password"
+          id="senha"
+          placeholder="Senha"
+          onChange={(e)=> setSenha(e.target.value)}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>Enter</button>
+    </form> 
+    <a href="">Esqueceu a senha?</a>
+    </div>
+     
+  );
+}
+  
+  export default Login
